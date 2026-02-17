418ce4915780f5f5b4e3560b0401150ec0bd70d2
rajdeep Majumdar <amazumder781@gmail.com>
2026-02-17 19:29:29 +0530

    create checkout and order models

---
+++ Backend/models/Order.js
@@ -0,0 +1,81 @@
+import mongoose from "mongoose";
+
+const orderItemSchema = new mongoose.Schema(
+  {
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    size: String,
+    color: String,
+    quantity: {
+      type: Number,
+      required: true,
+    },
+  },
+  { _id: false },
+);
+
+const orderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    orderItems: [orderItemSchema],
+    shippingAddress: {
+      address: { type: String, required: true },
+      city: { type: String, required: true },
+      postalCode: { type: String, required: true },
+      country: { type: String, required: true },
+    },
+    paymentMethod: {
+      type: String,
+      required: true,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+    isPaid: {
+      type: Boolean,
+      default: false,
+    },
+    paidAt: {
+      type: Date,
+    },
+    isDeliverd: {
+      type: Boolean,
+      default: false,
+    },
+    deliverdAt: {
+      type: Date,
+    },
+    paymentStatus: {
+      type: String,
+      default: "pending",
+    },
+    status: {
+      type: String,
+      enum: ["processing", "shipped", "deliverd", "cancelled"],
+      default: "processing",
+    },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("order", orderSchema);
