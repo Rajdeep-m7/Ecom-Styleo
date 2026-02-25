f01ba0e565c4eda21180344ba6378f1842f3b176
rajdeep Majumdar <amazumder781@gmail.com>
2026-02-25 09:02:52 +0530

    edit file import name thats gives an error during deployment

--- Backend/routes/adminProductRoute.js
+++ Backend/routes/adminProductRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import Product from "../models/Product.js";
+import Product from "../models/product.js";
 import { protect , admin } from "../middleware/authMiddleware.js";
 
 const adminProductRouter = express.Router();
