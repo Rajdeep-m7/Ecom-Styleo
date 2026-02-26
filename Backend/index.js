434598545c67208acd4b3984347e0f64879b525c
rajdeep Majumdar <amazumder781@gmail.com>
2026-02-26 20:47:06 +0530

    fix backend

--- Backend/index.js
+++ Backend/index.js
@@ -25,37 +25,18 @@ app.use(cors({
   credentials: true
 }));
 
-app.options("*", cors());
-
 app.use((req, res, next) => {
-
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://styleo.onrender.com"
-  );
-
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,POST,PUT,DELETE,OPTIONS"
-  );
-
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization"
-  );
-
-  res.setHeader(
-    "Access-Control-Allow-Credentials",
-    "true"
-  );
+  res.header("Access-Control-Allow-Origin", "https://styleo.onrender.com");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
 
   if (req.method === "OPTIONS") {
-    return res.status(200).end();
+    return res.sendStatus(200);
   }
 
   next();
 });
-
 
 const PORT = process.env.PORT;
 
